0f8cd0ed7d6061a9ea7679c01fb076f2bae69c50
Romi Julianto <romyjulians@gmail.com>
2022-09-26 00:12:21 +0700

    add: crud facility clinic

--- controllers/FacilityClinicController.js
+++ controllers/FacilityClinicController.js
@@ -8,3 +8,55 @@ export const getFacilityClinics = async (req, res) => {
         console.log(error.message);
     }
 }
+
+/* method getFacilityClinic By Id */
+export const getFacilityClinicById = async (req, res) => {
+    try {
+        const response = await facilityClinic.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method create Facility Clinic */
+export const createFacilityClinic = async (req, res) => {
+    try {
+        await facilityClinic.create(req.body);
+        res.status(201).json({ message: "Facility Clinic Created" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method update Facility Clinic */
+export const updateFacilityCllinic = async (req, res) => {
+    try {
+        await facilityClinic.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Facility Clinic Updated" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method delete Facility Clinic */
+export const deleteFacilityClinic = async (req, res) => {
+    try {
+        await facilityClinic.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Facility Clinic Deleted" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- routes/FacilityClinicRoute.js
+++ routes/FacilityClinicRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { getFacilityClinics } from "../controllers/FacilityClinicController.js";
+import {
+    getFacilityClinics,
+    getFacilityClinicById,
+    createFacilityClinic,
+    updateFacilityCllinic,
+    deleteFacilityClinic
+} from "../controllers/FacilityClinicController.js";
 
 /* inisiasi route */
 const router = express.Router();
 
 /* endpoint route */
 router.get('/facility_clinic', getFacilityClinics);
+router.get('/facility_clinic/:id', getFacilityClinicById);
+router.post('/facility_clinic/', createFacilityClinic);
+router.patch('/facility_clinic/:id', updateFacilityCllinic);
+router.delete('/facility_clinic/:id', deleteFacilityClinic);
 
 export default router;
