9c633821a1347f1735bdb1c9a5b1d5f3208c3ee2
Romi Julianto <romyjulians@gmail.com>
2022-09-25 22:55:56 +0700

    add: crud facility

--- controllers/FacilityController.js
+++ controllers/FacilityController.js
@@ -8,3 +8,55 @@ export const getFacilities = async (req, res) => {
         console.log(error.message);
     }
 }
+
+/* method getFacilities By Id */
+export const getFacilityById = async (req, res) => {
+    try {
+        const response = await Facility.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method create Facility */
+export const createFacility = async (req, res) => {
+    try {
+        await Facility.create(req.body);
+        res.status(201).json({ message: "Facility Clinic Created" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method update Facility */
+export const updateFacility = async (req, res) => {
+    try {
+        await Facility.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Facility Clinic Updated" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method delete Facility */
+export const deleteFacility = async (req, res) => {
+    try {
+        await Facility.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Facility Clinic Deleted" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- routes/FacilityRoute.js
+++ routes/FacilityRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { getFacilities } from "../controllers/FacilityController.js";
+import {
+    getFacilities,
+    getFacilityById,
+    createFacility,
+    updateFacility,
+    deleteFacility
+} from "../controllers/FacilityController.js";
 
 /* inisiasi route */
 const router = express.Router();
 
 /* endpoint route */
 router.get('/facilities', getFacilities);
+router.get('/facilities/:id', getFacilityById);
+router.post('/facilities/', createFacility);
+router.patch('/facilities/:id', updateFacility);
+router.delete('/facilities/:id', deleteFacility);
 
 export default router;
