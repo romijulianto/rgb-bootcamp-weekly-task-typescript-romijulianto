fcbb222454a4cc016ce2af0def6332f69bb9bd9e
Romi Julianto <romyjulians@gmail.com>
2022-09-25 23:28:15 +0700

    add: crud service

--- controllers/ServiceController.js
+++ controllers/ServiceController.js
@@ -8,3 +8,55 @@ export const getServices = async (req, res) => {
         console.log(error.message);
     }
 }
+
+/* method getService By Id */
+export const getServiceById = async (req, res) => {
+    try {
+        const response = await Service.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method create Service */
+export const createService = async (req, res) => {
+    try {
+        await Service.create(req.body);
+        res.status(201).json({ message: "Service Clinic Created" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method update Facility */
+export const updateService = async (req, res) => {
+    try {
+        await Service.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Service Clinic Updated" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method delete Facility */
+export const deleteService = async (req, res) => {
+    try {
+        await Service.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Service Clinic Deleted" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- routes/ServiceRoute.js
+++ routes/ServiceRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { getServices } from "../controllers/ServiceController.js";
+import {
+    getServices,
+    getServiceById,
+    createService,
+    updateService,
+    deleteService
+} from "../controllers/ServiceController.js";
 
 /* inisiasi route */
 const router = express.Router();
 
 /* endpoint route */
 router.get('/services', getServices);
+router.get('/services/:id', getServiceById);
+router.post('/services/', createService);
+router.patch('/services/:id', updateService);
+router.delete('/services/:id', deleteService);
 
 export default router;
