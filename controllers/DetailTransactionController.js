43f96cce6d1c77702b30456245d40970439eb75d
Romi Julianto <romyjulians@gmail.com>
2022-09-26 00:23:08 +0700

    add: crud detail transaction

--- controllers/DetailTransactionController.js
+++ controllers/DetailTransactionController.js
@@ -8,3 +8,55 @@ export const getDetailTransaction = async (req, res) => {
         console.log(error.message);
     }
 }
+
+/* method getDetailTransaction By Id */
+export const getDetailTransactionById = async (req, res) => {
+    try {
+        const response = await detailTransaction.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method create Detail Transaction */
+export const createDetailTransaction = async (req, res) => {
+    try {
+        await detailTransaction.create(req.body);
+        res.status(201).json({ message: "Detail Transaction Created" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method update Detail Transaction */
+export const updateDetailTransaction = async (req, res) => {
+    try {
+        await detailTransaction.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Detail Transaction Updated" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method delete Detail Transaction */
+export const deleteDetailTransaction = async (req, res) => {
+    try {
+        await detailTransaction.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Detail Transaction Deleted" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- routes/DetailTransactionRoute.js
+++ routes/DetailTransactionRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { getDetailTransaction } from "../controllers/DetailTransactionController.js";
+import {
+    getDetailTransaction,
+    getDetailTransactionById,
+    createDetailTransaction,
+    updateDetailTransaction,
+    deleteDetailTransaction
+} from "../controllers/DetailTransactionController.js";
 
 /* inisiasi route */
 const router = express.Router();
 
 /* endpoint route */
 router.get('/detail_transaction', getDetailTransaction);
+router.get('/detail_transaction/:id', getDetailTransactionById);
+router.post('/detail_transaction/', createDetailTransaction);
+router.patch('/detail_transaction/:id', updateDetailTransaction);
+router.delete('/detail_transaction/:id', deleteDetailTransaction);
 
 export default router;
