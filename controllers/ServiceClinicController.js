126cb47950975c44ffbd009d0344a2a98558c14a
Romi Julianto <romyjulians@gmail.com>
2022-09-25 23:45:31 +0700

    add: crud service_clinic

--- controllers/ServiceClinicController.js
+++ controllers/ServiceClinicController.js
@@ -8,3 +8,55 @@ export const getServiceClinics = async (req, res) => {
         console.log(error.message);
     }
 }
+
+/* method getServiceClinic By Id */
+export const getServiceClinicById = async (req, res) => {
+    try {
+        const response = await serviceClinic.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method create Service Clinic */
+export const createServiceClinic = async (req, res) => {
+    try {
+        await serviceClinic.create(req.body);
+        res.status(201).json({ message: "Service Clinic Created" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method update Service Clinic */
+export const updateServiceCllinic = async (req, res) => {
+    try {
+        await serviceClinic.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Service Clinic Updated" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+/* method delete Service Clinic */
+export const deleteServiceClinic = async (req, res) => {
+    try {
+        await serviceClinic.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Service Clinic Deleted" });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- routes/ServiceClinicRoute.js
+++ routes/ServiceClinicRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { getServiceClinics } from "../controllers/ServiceClinicController.js";
+import {
+    getServiceClinics,
+    getServiceClinicById,
+    createServiceClinic,
+    updateServiceCllinic,
+    deleteServiceClinic
+} from "../controllers/ServiceClinicController.js";
 
 /* inisiasi route */
 const router = express.Router();
 
 /* endpoint route */
 router.get('/service_clinic', getServiceClinics);
+router.get('/service_clinic/:id', getServiceClinicById);
+router.post('/service_clinic/', createServiceClinic);
+router.patch('/service_clinic/:id', updateServiceCllinic);
+router.delete('/service_clinic/:id', deleteServiceClinic);
 
 export default router;
